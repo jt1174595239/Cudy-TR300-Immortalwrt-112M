ee16df527cf4c07bf44e2764068b772356f669fe
jt1174595239 <70654910+jt1174595239@users.noreply.github.com>
2025-10-22 00:40:25 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,20 @@
 # See /LICENSE for more information.
 #
 
+# Modify ROM
+sed -i 's/4000000/7000000/g' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dts
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/TR3000/g' package/base-files/files/bin/config_generate
+
+# Modify timezone
+sed -i 's/GMT0/CST-8/g' package/base-files/files/bin/config_generate
+
+# Modify Zonename
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
